32ac9f1e5e6045e58d9a4346722ab37c3d413492
josueskt <77248411+josueskt@users.noreply.github.com>
2021-04-15 17:35:39 -0600

    1
    move and move xd

---
+++ Assets/scrips/moveplayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveplayer : MonoBehaviour
+{
+
+    public float horizonal_move;
+    public float vertical_move;
+    public CharacterController controler;
+    public float speed_v;
+    public Vector3 move_player;
+
+
+    
+    private Vector3 player_speed_imput; // limitar velocidad
+
+    // camera
+    public Camera cam;
+    private Vector3 cam_mira_recto;
+    private Vector3 cam_mira_derecha;
+
+    //gravedad 
+    public float gravedad = 9.8f;
+    public float fall_v;
+    public float jum_force;
+
+
+
+    
+    void Start()
+    {
+        controler = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        horizonal_move = Input.GetAxis("Horizontal");    
+        vertical_move = Input.GetAxis("Vertical");
+        player_speed_imput = new Vector3(horizonal_move, 0, vertical_move);
+       
+        player_speed_imput = Vector3.ClampMagnitude(player_speed_imput, 1);
+        camDirection();
+
+        move_player = player_speed_imput.x * cam_mira_derecha + player_speed_imput.z * cam_mira_recto;
+        move_player = move_player * speed_v;  // mobimiento del player
+        controler.Move(move_player * Time.deltaTime);
+
+        controler.transform.LookAt(controler.transform.position + move_player);
+
+      
+        PlayerSkills();
+    }
+    void camDirection()    //reotacion del player  con respeco a una cam
+    {
+        cam_mira_recto = cam.transform.forward;
+        cam_mira_derecha = cam.transform.right;
+
+        cam_mira_recto.y = 0;
+        cam_mira_derecha.y = 0;
+        cam_mira_recto = cam_mira_recto.normalized;
+        cam_mira_derecha = cam_mira_derecha.normalized;
+        
+    }
+
+    void PlayerSkills()
+    {
+        if (controler.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fall_v = jum_force;
+            move_player.y = fall_v;
+        }
+    }
+    
+
+
+   
+}
